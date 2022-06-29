5e415c433da4958708280601dffc1258b365d581
DefectingCat <i@defect.ink>
2022-06-29 14:47:31 +0800

    Add i18n

---
+++ components/HeadBar.tsx
@@ -0,0 +1,5 @@
+const HeadBar = () => {
+  return <></>;
+};
+
+export default HeadBar;
---
+++ layouts/MainLayout.tsx
@@ -0,0 +1,9 @@
+type Props = {
+  children: React.ReactNode;
+};
+
+const MainLayout = ({ children }: Props) => {
+  return <>{children}</>;
+};
+
+export default MainLayout;
