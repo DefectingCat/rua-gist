4a56b065f03d6b31fd53f93d89b3a4b4f8d0f5f3
DefectingCat <i@defect.ink>
2022-07-05 11:31:58 +0800

    Add query user info in server side

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import { GetServerSideProps } from 'next';
 import dynamic from 'next/dynamic';
 import nookies from 'nookies';
 import { ReactElement } from 'react';
+import prisma from 'lib/PrismaClient';
 
 const MainLayout = dynamic(() => import('layouts/MainLayout'));
 
@@ -14,29 +15,40 @@ const Home = () => {
 };
 
 export const getServerSideProps: GetServerSideProps = async ({ req }) => {
-  const cookies = nookies.get({ req });
-  const token = cookies.token;
-  if (!token)
-    return {
-      props: {},
-    };
+  try {
+    const cookies = nookies.get({ req });
+    const token = cookies.token;
+    if (!token) throw new Error('No token');
 
-  const { payloadObj }: { payloadObj: any } = decodeJWT(token);
-  if (!payloadObj)
-    return {
-      props: {},
-    };
+    const { payloadObj }: { payloadObj: any } = decodeJWT(token);
+    if (!payloadObj) throw new Error('Token parse error!');
 
-  return {
-    props: {
-      initialState: {
-        users: {
-          logined: true,
-          ...payloadObj.data,
+    const user = await prisma.user.findUnique({
+      where: {
+        email: payloadObj.data.email,
+      },
+    });
+    if (!user) throw new Error('User in token was not found!');
+
+    const { id: _id, password: _password, ...returnUser } = user;
+    return {
+      props: {
+        initialState: {
+          users: {
+            logined: true,
+            ...returnUser,
+          },
         },
       },
-    },
-  };
+    };
+  } catch (e) {
+    console.log(e);
+    return {
+      props: {},
+    };
+  } finally {
+    prisma.$disconnect();
+  }
 };
 
 Home.getLayout = function getLayout(page: ReactElement) {
